41b4719e7bce952e2ae5ad921ed53496144cf546
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-31 19:42:44 -0400

    UI setup - add components (timer, buttons, label

--- main.py
+++ main.py
@@ -21,11 +21,24 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+timer_label = Label(text="Timer", font=(FONT_NAME, 50, "normal"), fg=GREEN, bg=YELLOW)
+timer_label.grid(column=1, row=0)
+
 canvas= Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img )
-canvas.create_text(100,130,text="00:00", fill="white", font=("Courier", 35, "bold"))
-canvas.pack()
+canvas.create_text(100,130,text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+
+start_button = Button(text="Start",highlightthickness=0, highlightbackground=YELLOW)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset",highlightthickness=0, highlightbackground=YELLOW)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text="✓", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3 )
 
 
 window.mainloop()
