b28be9d18567a82ceabb41061fa15427b570de7e
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-06-01 19:18:23 -0400

    reset mechanism

--- main.py
+++ main.py
@@ -8,13 +8,22 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
+
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
+    
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     global reps
@@ -46,7 +55,8 @@ def count_down(count):
 
     canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000,count_down,count-1)
+        global timer
+        timer = window.after(1000,count_down,count-1)
     else:
         start_timer()
         # Show check after user every sucessful work session
@@ -73,7 +83,7 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start",highlightthickness=0, highlightbackground=YELLOW, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset",highlightthickness=0, highlightbackground=YELLOW)
+reset_button = Button(text="Reset",highlightthickness=0, highlightbackground=YELLOW, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 check_marks = Label(fg=GREEN, bg=YELLOW)
